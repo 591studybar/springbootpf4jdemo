9bf579ee0178a4ed782c16cbf120c83b74ddd278
sharplee <497906694@qq.com>
2020-03-22 20:13:25 +0800

    testlistener

---
+++ src/main/java/com/example/pf4jdemo/listener/TestListener.java
@@ -0,0 +1,13 @@
+package com.example.pf4jdemo.listener;
+
+/**
+ * @Author sharplee
+ * @Date 2020/3/22 20:12
+ * @Version 1.0
+ * @PackageName com.example.pf4jdemo.listener
+ * @ClassName TestListener
+ * @JavaFile com.example.pf4jdemo.listener.TestListener.java
+ */
+public class TestListener {
+
+}
---
+++ src/test/java/com/example/pf4jdemo/Pf4jdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pf4jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pf4jdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
